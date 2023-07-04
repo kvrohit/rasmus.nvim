61c473a09f2ca337b9cc47b84af0bcf23dcc0842
Mohammad Reza Karimi <mkarimi@inf.ethz.ch>
2023-07-04 15:00:23 +0200

    update to recent neovim lua api
    Instead of using `vim.api.nvim_command` use `vim.api.nvim_set_hl` directly.
    This involves changing the way highlight groups are managed slightly.

--- lua/rasmus/utils.lua
+++ lua/rasmus/utils.lua
@@ -1,11 +1,7 @@
 local M = {}
 
 M.highlight = function(group, color)
-  local style = color.style and "gui=" .. color.style or "gui=NONE"
-  local fg = color.fg and "guifg=" .. color.fg or "guifg=NONE"
-  local bg = color.bg and "guibg=" .. color.bg or "guibg=NONE"
-  local sp = color.sp and "guisp=" .. color.sp or ""
-  vim.api.nvim_command("highlight " .. group .. " " .. style .. " " .. fg .. " " .. bg .. " " .. sp)
+  vim.api.nvim_set_hl(0, group, color)
 end
 
 return M
